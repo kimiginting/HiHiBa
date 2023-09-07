72be86e975515ff44e55efc3682522341f84d077
hafidzmrizky <hafidzmrizky@icloud.com>
2023-09-07 22:22:11 +0700

    add: Calculate.java

---
+++ app/src/main/java/com/example/hihiba/Calculate.java
@@ -0,0 +1,41 @@
+package com.example.hihiba;
+
+public class Calculate {
+    private double diameter, jarijari;
+    private String perintah;
+
+    public Calculate(double diameter, double jarijari, String perintah) {
+        this.diameter = diameter;
+        this.jarijari = jarijari;
+        this.perintah = perintah;
+    }
+
+    public double diameterToJarijari() {
+        return diameter / 2;
+    }
+
+    public double cariLuas() {
+        return Math.PI * jarijari * jarijari;
+    }
+
+    public double cariKeliling() {
+        return 2 * Math.PI * jarijari;
+    }
+
+    public double hitung() {
+        if (perintah.equals("luas")) {
+            return cariLuas();
+        } else if (perintah.equals("keliling")) {
+            return cariKeliling();
+        } else {
+            return 0;
+        }
+    }
+
+
+
+
+
+
+
+}
